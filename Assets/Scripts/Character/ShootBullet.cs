ac3f601df8ccd0b793925f92341ec55b727127d5
FrumosUniverse <58672300+FrumosUniverse@users.noreply.github.com>
2023-03-30 00:41:13 -0700

    Bullet

---
+++ Assets/Scripts/Character/ShootBullet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour
+{
+    public void Shoot(int fireRate, int ammoLeft, int damage)
+    {
+        
+    }
+}
