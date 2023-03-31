ec3d98ef68f028dcc0b56a9977a1a8c48ffdc438
Vlad <pedogfd@gmail.com>
2023-03-31 13:39:58 +0800

    Updated player input handler
    -Changed input
    -added function that shows if cursor is on the right side or left side

--- Assets/Scripts/Character/PlayerInputHandler.cs
+++ Assets/Scripts/Character/PlayerInputHandler.cs
@@ -4,24 +4,63 @@ using UnityEngine;
 
 public class PlayerInputHandler : MonoBehaviour
 {
+    public Transform player;
+    private bool isCursorOnRightSide = false; // variable to store whether cursor is on right side
+    private bool wasCursorOnRightSide = false; // variable to store previous state of cursor
+
     private Vector2 input;
     private CharacterHandler handler;
+
     private void Start()
     {
+
         handler = GetComponent<CharacterHandler>();
-        
+
+
     }
     // Update is called once per frame
     void Update()
     {
+
         input = new Vector2(Input.GetAxis("Horizontal") , Input.GetAxis("Vertical"));
         if (Input.GetButtonDown("Fire1"))
         {
             handler.AttackRequest();
         }
-        if (input!=Vector2.zero)
+        if (input != Vector2.zero || input == Vector2.zero)
         {
             handler.MoveRequestDirection(input);
-         }
+        }
+
+        // get the x-coordinate of the cursor
+        float cursorX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+
+        // get the x-coordinate of the character
+        float playerX = player.position.x;
+
+        // compare the two coordinates to determine if cursor is on right or left side
+        if (cursorX > playerX)
+        {
+            isCursorOnRightSide = true;
+        }
+        else
+        {
+            isCursorOnRightSide = false;
+        }
+
+        // check if cursor has moved to a different side than before
+        if (isCursorOnRightSide != wasCursorOnRightSide)
+        {
+            // call the ChangeLookingSide function with the new side
+            ChangeLookingSide(isCursorOnRightSide);
+        }
+
+        // update the previous state of the cursor
+        wasCursorOnRightSide = isCursorOnRightSide;
+    }
+    void ChangeLookingSide(bool isLookingRight)
+    {
+        // do something here  based on the new side
+        Debug.Log(Input.mousePosition);
     }
 }
